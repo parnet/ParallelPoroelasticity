53114eb14ab430694e8e2c7f7cf5bac23f2bbdfa
parnet <parnet@stud.uni-frankfurt.de>
2022-01-08 08:50:19 +0100

    corrected skip down

--- scripts/parallel_biot_driver.lua
+++ scripts/parallel_biot_driver.lua
@@ -490,7 +490,7 @@ if (XARGS.p_method == "SEQSimple") then
     time_disc = ThetaTimeStep(domainDiscT)
     time_disc:set_theta(1)
     integrator = SimpleTimeIntegrator(time_disc)
-    integration:set_solver(nlsolver)
+    integrator:set_solver(nlsolver)
 
     print("setup done ")
     time = BraidTimer()
